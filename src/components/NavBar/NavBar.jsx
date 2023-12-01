270cc10cbd5d59f1ed377e163d2d6c6db0bd3fdd
ardalanmemar93 <ardalanmemar@gmail.com>
2023-12-01 13:29:07 -0800

    adde br that i will change down the line to the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,10 +10,11 @@ const NavBar = ({ user, setUser }) => {
 
   return (
     <nav className="bg--500 p-4 text-white neon">
+      <span className="ml-2">Welcome, {user.name}</span>
+      <br />
       <Link to="/squads" className="hover:text-gray-300">Squads</Link>
       <span className="mx-2">|</span>
       <Link to="/squads/new" className="hover:text-gray-300">New Squad</Link>
-      <span className="ml-2">Welcome, {user.name}</span>
       <span className="ml-2"><Link to="" onClick={handleLogOut} className="hover:text-gray-300">Log Out</Link></span>
     </nav>
   );
