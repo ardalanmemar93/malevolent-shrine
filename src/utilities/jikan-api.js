792801d50cb74241eece02188a37fa8c57616e08
ardalanmemar93 <ardalanmemar@gmail.com>
2023-12-01 14:09:03 -0800

    changed fetch to axios

--- src/utilities/jikan-api.js
+++ src/utilities/jikan-api.js
@@ -1,12 +1,16 @@
+import axios from 'axios';
 const BASE_URL = 'https://api.jikan.moe/v4';
 
 
 export default async function getCharacter(id) {
     const url = `${BASE_URL}/characters/${id}`;
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await axios(url);
+    const data = response.data;
     console.log(data.data)
     console.log(data.data.images.jpg.image_url)
+    console.log(data.data.name_kanji)
+    console.log(data.data.about)
+    console.log(data.data.favorites)
     return data.data;
     };
 
