5130b86c586841e57f3cbfa3b010a93440ea1f81
ardalanmemar93 <ardalanmemar@gmail.com>
2023-12-06 20:34:25 -0800

    doing some styling

--- src/pages/SquadPage/SquadPage.jsx
+++ src/pages/SquadPage/SquadPage.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import sendRequest from '../../utilities/send-request';
-
-
+import { animated, useSpring } from 'react-spring';
 
 const SquadPage = () => {
   const [userSquads, setUserSquads] = useState([]);
@@ -12,12 +11,9 @@ const SquadPage = () => {
 
   const BASE_URL = '/api/squads';
 
-  // Define fetchSquads outside the useEffect
   const fetchSquads = async () => {
     try {
-      // Send the GET request to the server
       const res = await sendRequest(BASE_URL);
-      // Set the list of squads in state
       setUserSquads(res);
     } catch (error) {
       console.error('Error fetching user squads:', error);
@@ -25,122 +21,127 @@ const SquadPage = () => {
   };
 
   useEffect(() => {
-    // Invoke the async function
     fetchSquads();
   }, []);
 
   const handleDeleteSquad = async (squadId) => {
     try {
-      // Send the DELETE request to the server
       await sendRequest(`${BASE_URL}/${squadId}`, 'DELETE');
-      // Update state by removing the squad
-      setUserSquads(userSquads.filter(squad => squad._id !== squadId));
+      setUserSquads(userSquads.filter((squad) => squad._id !== squadId));
     } catch (error) {
       console.error('Error deleting squad:', error);
     }
   };
 
-
-
   const handleSquadClick = async (squadId) => {
     try {
-      // Send the GET request to the server
       const squad = await sendRequest(`${BASE_URL}/${squadId}`);
-      // Clear the selected squad if the same squad is clicked twice
       if (selectedSquad && selectedSquad._id === squad._id) {
         setSelectedSquad(null);
+        setSelectedCharacter(null);
         return;
       }
-      // Set the selected squad in state
       setSelectedSquad(squad);
-
     } catch (error) {
       console.error('Error fetching squad:', error);
     }
   };
 
   const handleCharacterClick = (character) => {
-    // Toggle the visibility of the selected character when it is clicked
     setSelectedCharacter((prevSelectedCharacter) => {
       if (prevSelectedCharacter && prevSelectedCharacter._id === character._id) {
-        return null; // Hide the details if the same character is clicked again
+        return null;
       }
-      return character; // Show the details if a different character is clicked
+      return character;
     });
   };
-  
-
 
   const handleUpdateSquad = (squadId) => {
-    // Navigate to the squad form for updating
     navigate(`/update-squad/${squadId}`);
   };
-  
 
+  const aboutSpring = useSpring({
+    from: { opacity: 0, height: 0 },
+    to: { opacity: 1, height: 'auto' },
+  });
 
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4 neon-green">Your Squads</h2>
-      <ul>
-        {userSquads.map((squad) => (
-          <li key={squad._id} className="mb-2">
-            <div
-              className="flex items-center justify-between neon-blue p-2 rounded cursor-pointer"
-              onClick={() => handleSquadClick(squad._id)}
-            >
-              <span className="text-lg">{squad.name}</span>
+    <div className="form-wrapper w-full  p-6 shadow-md rounded-md">
+    <h2 className="text-2xl font-bold mb-4 neon-green">Your Squads</h2>
+     <ul className="space-y-4">
+  {userSquads.map((squad) => (
+    <li key={squad._id} className="mb-2">
+      <div
+        className="flex items-center justify-between p-4 rounded cursor-pointer bg-neon-blue"
+        onClick={() => handleSquadClick(squad._id)}
+      >
+        <span className="text-lg neon-blue">{squad.name}</span>
 
-              <button
-                onClick={() => handleUpdateSquad(squad._id)}
-                className="bg-yellow-500 text-white px-4 py-2 rounded mr-2 hover:bg-yellow-600"
-              >
-                Update
-              </button>
-              <button
-                onClick={() => handleDeleteSquad(squad._id)}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-              >
-                Delete
-              </button>
-            </div>
+        <div className="space-x-2">
+          <button
+            onClick={() => handleUpdateSquad(squad._id)}
+            className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 text-sm"
+          >
+            Update
+          </button>
+          <button
+            onClick={() => handleDeleteSquad(squad._id)}
+            className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 text-sm"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
 
-            {/* Display squad details if selected */}
-            {selectedSquad && selectedSquad._id === squad._id && (
-              <div className=" p-4 mt-2 rounded">
-                <h3 className="text-lg font-bold mb-2 neon-white">{selectedSquad.name} Characters</h3>
-                <ul>
-                  {selectedSquad.characters.map((character) => (
-                    <li key={character._id} className="mb-4">
-                      <div className="flex items-center bg-teal-500">
-                        <img
-                          src={character.imageUrl}
-                          alt={character.name}
-                          className="w-14 h-14 rounded-full mr-4"
-                          onClick={() => handleCharacterClick(character)}
-                          // Add an onClick handler to show character details
-                        />
-                        <div>
-                          <p className="font-bold">{character.name}</p>
-                          <p className="text-gray-600">{character.nameKanji}</p>
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
-
-      {/* Display selected character details */}
-      {selectedCharacter && (
-        <div className="bg-gray-200 p-4 mt-2 rounded">
-          <h3 className="text-lg font-bold mb-2">{selectedCharacter.name} Details</h3>
-          <p>{selectedCharacter.about}</p>
-          {/* Add other character details you want to display */}
+      {selectedSquad && selectedSquad._id === squad._id && (
+        <div className="p-4 mt-2 rounded bg-gray-200">
+          <h3 className="text-lg font-bold mb-2 text-neon-white">{selectedSquad.name} Characters</h3>
+          <ul className="space-y-4">
+            {selectedSquad.characters.map((character) => (
+              <li key={character._id} className="mb-4">
+                <div className="flex items-center bg-teal-500 p-4 rounded">
+                  <img
+                    src={character.imageUrl}
+                    alt={character.name}
+                    className="w-20 h-20 rounded-full mr-4 cursor-pointer"
+                    onClick={() => handleCharacterClick(character)}
+                  />
+                  <div>
+                    <p className="font-bold text-white">{character.name}</p>
+                    <p className="text-gray-600">{character.nameKanji}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       )}
+    </li>
+  ))}
+</ul>
+
+
+  {selectedCharacter && (
+    <div className="bg-gray-200 p-4 mt-2 rounded">
+      <div className="flex items-center">
+        <div className="w-1/3 mr-4">
+          <img
+            src={selectedCharacter.imageUrl}
+            alt={selectedCharacter.name}
+            className="w-full h-auto rounded-full cursor-pointer"
+            onClick={() => handleCharacterClick(selectedCharacter)}
+          />
+        </div>
+        <div className="w-2/3">
+          <h3 className="text-lg font-bold mb-2">{selectedCharacter.name} Details</h3>
+          <animated.div style={aboutSpring}>
+            <p>{selectedCharacter.about}</p>
+          </animated.div>
+        </div>
+      </div>
+    </div>
+  )}
+
     </div>
   );
 };
